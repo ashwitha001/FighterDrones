160431a11d805becd68f8091c07f8e5997859d5a
viiachristina <christinadang@cmail.carleton.ca>
2025-01-31 15:14:38 -0500

    Added CoordinatesTest.java class and MessageTest.java class

---
+++ src/CoordinatesTest.java
@@ -0,0 +1,37 @@
+package src;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoordinatesTest {
+    Coordinates coords;
+    @BeforeEach
+    void setUp(){
+       coords  = new Coordinates(100,200,300,400);
+    }
+
+    @Test
+    void testX1(){
+    assertEquals(100,coords.getX1());
+    }
+
+    @Test
+    void testY1(){
+        assertEquals(200,coords.getY1());
+    }
+
+    @Test
+    void testX2(){
+        assertEquals(300,coords.getX2());
+    }
+
+    @Test
+    void testY2(){
+        assertEquals(400,coords.getY2());
+    }
+
+    @Test
+    void testCoordTS(){
+        assertEquals("(100, 200) to (300, 400)",coords.toString());
+    }
+}
---
+++ src/MessageTest.java
@@ -0,0 +1,51 @@
+package src;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MessageTest {
+    Message msg;
+    LocalTime localTime;
+    String timeString;
+
+    @BeforeEach
+    void setUp(){
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        timeString = "14:50:45";
+        localTime = LocalTime.parse(timeString, timeFormatter);
+        msg  = new Message("ACTIVE_FIRE",1,"HIGH", localTime, timeString);
+    }
+
+    @Test
+    void testType(){
+        assertEquals("ACTIVE_FIRE",msg.getType());
+    }
+
+    @Test
+    void testID(){
+        assertEquals(1,msg.getZoneID());
+    }
+
+    @Test
+    void testSeverity(){
+        assertEquals("HIGH",msg.getSeverity());
+    }
+
+    @Test
+    void testEventTime(){
+        assertEquals(localTime,msg.getEventTime());
+    }
+
+    @Test
+    void testTimeString(){
+        assertEquals(timeString,msg.getEventTimeString());
+    }
+
+    @Test
+    void testMessageString(){
+        String expectedString = "Message{time='14:50:45', type='ACTIVE_FIRE', zoneID=1, severity='HIGH'}";
+        assertEquals(expectedString,msg.toString());
+    }
+}
