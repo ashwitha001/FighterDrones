6dc75e3b65e84d79108a86f0b06ab1079adb4fdd
viiachristina <christinadang@cmail.carleton.ca>
2025-02-02 12:38:17 -0500

    Added SchedulerTest.java class and FireIncidentSubsystemTest.java class

---
+++ src/test/java/src/test/SchedulerTest.java
@@ -0,0 +1,37 @@
+package src.test.java.src.test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import src.Message;
+import src.Scheduler;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SchedulerTest {
+    Thread schedulerThread;
+
+    @BeforeEach
+    public void setUp(){
+        BlockingQueue<Message> incidentQueue = new LinkedBlockingQueue<>();
+        BlockingQueue<Message> dronesQueue = new LinkedBlockingQueue<>();
+        BlockingQueue<Message> droneCompletionQueue = new LinkedBlockingQueue<>();
+        BlockingQueue<Message> incidentCompletionQueue = new LinkedBlockingQueue<>();
+
+        schedulerThread = new Thread(new Scheduler(incidentQueue, dronesQueue, droneCompletionQueue, incidentCompletionQueue), "Scheduler");
+
+    }
+    @Test
+    public void test(){
+        schedulerThread.start();
+
+        try {
+            Thread.sleep(10000); // 10 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(schedulerThread.isAlive());
+    }
+
+}
